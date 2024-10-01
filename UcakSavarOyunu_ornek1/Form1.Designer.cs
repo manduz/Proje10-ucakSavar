91ae2778688595df994100ebcafa6561787ac072
manduz <emekmanduz3623@gmail.com>
2024-10-01 21:39:14 +0300

    UcakSavar Play

---
+++ UcakSavarOyunu_ornek1/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace UcakSavarOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Score = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // Score
+            // 
+            this.Score.AutoSize = true;
+            this.Score.Location = new System.Drawing.Point(24, 19);
+            this.Score.Name = "Score";
+            this.Score.Size = new System.Drawing.Size(29, 13);
+            this.Score.TabIndex = 0;
+            this.Score.Text = "Skor";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(59, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(10, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "-";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(758, 546);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Score);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Score;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+    }
+}
+
---
+++ UcakSavarOyunu_ornek1/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using UcakSavarOyunu.Properties;
+
+namespace UcakSavarOyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        PictureBox ucakSavar = new PictureBox();
+        PictureBox ucak = new PictureBox();
+        PictureBox mermi = new PictureBox();
+
+        ArrayList mermiList = new ArrayList();
+        ArrayList dusmanUcak = new ArrayList();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            ucakSavar.Image = Resources.ucakSavar;
+            this.Controls.Add(ucakSavar);
+            ucakSavar.Location = new Point(300, 300);
+            ucakSavar.Size = new Size(300, 300);
+            timer1.Enabled = true;
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            dusmanUcak.Add(ucakUret());
+
+            foreach (PictureBox item in dusmanUcak)
+            {
+                ucakHareket(item);
+            }
+        }
+
+        private void ucakHareket(PictureBox item)
+        {
+            ucak.Image = Resources.ucak;
+            int x = ucak.Location.X;
+            int y = ucak.Location.Y;
+            y = y + 5;
+            ucak.Location = new Point(x, y);
+            this.Controls.Add(ucak);
+        }
+
+        private object ucakUret()
+        {
+            PictureBox ucak = new PictureBox();
+            ucak.Image = Resources.ucak;
+            Random random = new Random();
+            int ucakBaslat = random.Next(1000);
+            ucak.Location = new Point(ucakBaslat,0);
+            return ucak;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            ucakSavarHareket(e);
+            if(e.KeyCode == Keys.Space)
+            {
+                mermiList.Add(MermiUret());
+                timer2.Enabled = true;
+            }
+        }
+
+        private object MermiUret()
+        {
+            PictureBox mermi = new PictureBox();
+            mermi.Image = Resources.mermitek;
+            mermi.SizeMode = PictureBoxSizeMode.StretchImage;
+            mermi.Location = new Point(ucakSavar.Location.X,ucakSavar.Location.Y);
+            this.Controls.Add(mermi);
+            return mermi;
+        }
+
+        private void ucakSavarHareket(KeyEventArgs e)
+        {
+           int ucakSavarx = ucakSavar.Location.X;
+           int ucakSavary = ucakSavar.Location.Y;
+            if (e.KeyCode == Keys.Right)
+            {
+                ucakSavarx += 5;
+            }
+            else if (e.KeyCode == Keys.Left)
+            {
+                ucakSavarx -= 5;
+            }
+            ucakSavar.Location = new Point(ucakSavarx, ucakSavary);
+                this.Controls.Add(ucakSavar);
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            foreach (PictureBox item in mermiList)
+            {
+                mermiHareket(item);
+            }
+        }
+
+        private void mermiHareket(PictureBox mermi)//item = mermi
+        {
+            mermi.Image = Resources.mermi;
+            int x = mermi.Location.X;
+            int y = mermi.Location.Y;
+            y = y - 5; //mermi yukarı çıkacak
+            mermi.Location = new Point(x, y);
+            this.Controls.Add(mermi);
+        }
+
+        private int sayi = 0;
+        PictureBox kaldirilanUcaklar =  new PictureBox();
+        PictureBox kaldirilanMermiler = new PictureBox();
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            foreach (PictureBox item in mermiList)
+            {
+                foreach (PictureBox item1 in dusmanUcak)
+                {
+                    if (item.Bounds.IntersectsWith(item1.Bounds))
+                    {
+                        this.Controls.Remove(item);
+                        this.Controls.Remove(item1);
+                        kaldirilanMermiler = item;
+                        kaldirilanUcaklar = item1;
+                        sayi++;
+                        label2.Text = sayi.ToString();
+                    }
+                }
+            }
+            mermiList.Remove(kaldirilanMermiler);
+            dusmanUcak.Remove(kaldirilanUcaklar);
+        }
+    }
+}
